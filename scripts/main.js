79d6661699ec4f73958c7ef58102988060832313
SoyMilk13 <119906029+SoyMilk13@users.noreply.github.com>
2023-01-04 16:34:57 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,148 @@
-const playButton = document.getElementById('playButton');
-
-playButton.onclick = () => {
-    const actions = ['rock', 'fire', 'scissors', 'snake', 'human', 'tree', 'wolf', 'sponge', 'paper', 'air', 'water', 'dragon', 'devil', 'lightning', 'gun'];
-    const options = ['Yes', 'yes', 'Y', 'y'];
-
-    function credit() {
-        alert('Rock Paper Scissors Two: A minigame made by Soy Milk.')
-        alert('Please read "How to Play?" for instructions.')
+function _0x541c(_0x5ae325, _0xeea544) {
+    const _0x596e32 = _0x596e();
+    return _0x541c = function (_0x541c52, _0x350346) {
+        _0x541c52 = _0x541c52 - 0x144;
+        let _0x3b7ee9 = _0x596e32[_0x541c52];
+        return _0x3b7ee9;
+    }, _0x541c(_0x5ae325, _0xeea544);
+}
+const _0x520373 = _0x541c;
+function _0x596e() {
+    const _0x515518 = [
+        'sponge',
+        'indexOf',
+        '16288exyeQQ',
+        '740SgkROi',
+        '420HNUrGa',
+        '11255870XxHtYK',
+        'You\x20lose!',
+        'Invalid\x20choice!',
+        '4bliAah',
+        '242163wPQoou',
+        '338889IyZajU',
+        'onclick',
+        'random',
+        'Your\x20opponent\x20chose\x20',
+        'Your\x20choice:\x20',
+        'rock',
+        'Please\x20read\x20\x22How\x20to\x20Play?\x22\x20for\x20instructions.',
+        'paper',
+        '488mKvsMZ',
+        'length',
+        'scissors',
+        'getElementById',
+        'You\x20chose\x20',
+        'Rock\x20Paper\x20Scissors\x20Two:\x20A\x20minigame\x20made\x20by\x20Soy\x20Milk.',
+        'Yes',
+        '30sghDNc',
+        'devil',
+        '1317694FFgnGy',
+        'dragon',
+        'lightning',
+        'water',
+        'Restarting...',
+        '38673wybEJO',
+        'gun',
+        'wolf',
+        'You\x20win!',
+        'air',
+        'Ending\x20game...',
+        '336895eGiVbo',
+        'toLowerCase',
+        'Would\x20you\x20like\x20to\x20play\x20again?',
+        'playButton',
+        'yes',
+        '11YtUflO',
+        'Tie\x20game!'
+    ];
+    _0x596e = function () {
+        return _0x515518;
     };
-    credit();
-
-    let restart = true;
-    let playAgain = null;
-    let playAgainIndex = -1;
-    function restartGame() {
-        playAgain = prompt('Would you like to play again?');
-        playAgain = playAgain.toLowerCase();
-        playAgainIndex = options.indexOf(playAgain);
-        if (playAgainIndex > 0) {
-            alert('Restarting...')
-        } else {
-            alert('Ending game...')
-            return restart = false;
-        };
-        return playAgain;
-    };
-
-    while (restart == true) {
-        let userChoice = null;
-        let userChoiceIndex = -1;
+    return _0x596e();
+}
+(function (_0x312fa4, _0x33b40b) {
+    const _0x21aa9b = _0x541c, _0x506999 = _0x312fa4();
+    while (!![]) {
+        try {
+            const _0x406844 = parseInt(_0x21aa9b(0x169)) / 0x1 + -parseInt(_0x21aa9b(0x167)) / 0x2 * (-parseInt(_0x21aa9b(0x168)) / 0x3) + -parseInt(_0x21aa9b(0x161)) / 0x4 * (-parseInt(_0x21aa9b(0x162)) / 0x5) + parseInt(_0x21aa9b(0x14b)) / 0x6 * (-parseInt(_0x21aa9b(0x14d)) / 0x7) + parseInt(_0x21aa9b(0x144)) / 0x8 * (parseInt(_0x21aa9b(0x152)) / 0x9) + -parseInt(_0x21aa9b(0x164)) / 0xa * (-parseInt(_0x21aa9b(0x15d)) / 0xb) + parseInt(_0x21aa9b(0x163)) / 0xc * (-parseInt(_0x21aa9b(0x158)) / 0xd);
+            if (_0x406844 === _0x33b40b)
+                break;
+            else
+                _0x506999['push'](_0x506999['shift']());
+        } catch (_0x33f414) {
+            _0x506999['push'](_0x506999['shift']());
+        }
+    }
+}(_0x596e, 0x9cd98));
+const playButton = document[_0x520373(0x147)](_0x520373(0x15b));
+playButton[_0x520373(0x16a)] = () => {
+    const _0x274e9 = _0x520373, _0x39f822 = [
+            _0x274e9(0x16e),
+            'fire',
+            _0x274e9(0x146),
+            'snake',
+            'human',
+            'tree',
+            _0x274e9(0x154),
+            _0x274e9(0x15f),
+            _0x274e9(0x170),
+            _0x274e9(0x156),
+            _0x274e9(0x150),
+            _0x274e9(0x14e),
+            _0x274e9(0x14c),
+            _0x274e9(0x14f),
+            _0x274e9(0x153)
+        ], _0x16565b = [
+            _0x274e9(0x14a),
+            _0x274e9(0x15c),
+            'Y',
+            'y'
+        ];
+    function _0x1425ea() {
+        const _0x89b47c = _0x274e9;
+        alert(_0x89b47c(0x149)), alert(_0x89b47c(0x16f));
+    }
+    ;
+    _0x1425ea();
+    let _0x1b7d1c = !![], _0x415ec8 = null, _0x2fef4a = -0x1;
+    function _0x33846c() {
+        const _0x2c0a85 = _0x274e9;
+        _0x415ec8 = prompt(_0x2c0a85(0x15a)), _0x415ec8 = _0x415ec8[_0x2c0a85(0x159)](), _0x2fef4a = _0x16565b[_0x2c0a85(0x160)](_0x415ec8);
+        if (_0x2fef4a > 0x0)
+            alert(_0x2c0a85(0x151));
+        else
+            return alert(_0x2c0a85(0x157)), _0x1b7d1c = ![];
+        ;
+        return _0x415ec8;
+    }
+    ;
+    while (_0x1b7d1c == !![]) {
+        let _0x1c5b79 = null, _0x5ccd07 = -0x1;
         do {
-            userChoice = prompt('Your choice: ');
-            userChoice = userChoice.toLowerCase();
-            userChoiceIndex = actions.indexOf(userChoice);
-            if (userChoiceIndex < 0) {
-                alert('Invalid choice!');
-            };
-        } while (userChoiceIndex < 0);
-
-        const opChoice = getRandomArrayValue(actions);
-        const opChoiceIndex = actions.indexOf(opChoice);
-
-        alert(`You chose ${userChoice}.`, userChoiceIndex + 1);
-        alert(`Your opponent chose ${opChoice}.`, opChoiceIndex + 1);
-        checkTie();
-
-        function chooseWinner() {
-            const withinSeven = (opChoiceIndex > userChoiceIndex && opChoiceIndex <= (userChoiceIndex + 7));
-            const withinSevenWrapped = (withinSeven || opChoiceIndex <= (userChoiceIndex - 8));
-
-            const userWon = (userChoiceIndex <= 7) ? withinSeven : withinSevenWrapped;
-            alert(userWon ? 'You win!' : 'You lose!');
-        };
-
-        function checkTie() {
-            if (userChoiceIndex == opChoiceIndex) {
-                alert('Tie game!');
-                return restartGame();
-            };
-            chooseWinner();
-            restartGame();
-        };
-
-        function getRandomArrayValue(actions) {
-            return actions[Math.floor(Math.random() * actions.length)];
-        };
-    };
+            _0x1c5b79 = prompt(_0x274e9(0x16d)), _0x1c5b79 = _0x1c5b79['toLowerCase'](), _0x5ccd07 = _0x39f822[_0x274e9(0x160)](_0x1c5b79);
+            _0x5ccd07 < 0x0 && alert(_0x274e9(0x166));
+            ;
+        } while (_0x5ccd07 < 0x0);
+        const _0x20b52a = _0x3478c8(_0x39f822), _0x136a5b = _0x39f822[_0x274e9(0x160)](_0x20b52a);
+        alert(_0x274e9(0x148) + _0x1c5b79 + '.', _0x5ccd07 + 0x1), alert(_0x274e9(0x16c) + _0x20b52a + '.', _0x136a5b + 0x1), _0x1aa4bc();
+        function _0x4f441e() {
+            const _0x1f6781 = _0x274e9, _0x3573a1 = _0x136a5b > _0x5ccd07 && _0x136a5b <= _0x5ccd07 + 0x7, _0x36aa8c = _0x3573a1 || _0x136a5b <= _0x5ccd07 - 0x8, _0x3d2b86 = _0x5ccd07 <= 0x7 ? _0x3573a1 : _0x36aa8c;
+            alert(_0x3d2b86 ? _0x1f6781(0x155) : _0x1f6781(0x165));
+        }
+        ;
+        function _0x1aa4bc() {
+            const _0xe85f24 = _0x274e9;
+            if (_0x5ccd07 == _0x136a5b)
+                return alert(_0xe85f24(0x15e)), _0x33846c();
+            ;
+            _0x4f441e(), _0x33846c();
+        }
+        ;
+        function _0x3478c8(_0x1ec591) {
+            const _0x49c4b0 = _0x274e9;
+            return _0x1ec591[Math['floor'](Math[_0x49c4b0(0x16b)]() * _0x1ec591[_0x49c4b0(0x145)])];
+        }
+        ;
+    }
+    ;
 };
