e16f181fe2be754667e8ee8ab2df084a59fc6751
SoyMilk13 <119906029+SoyMilk13@users.noreply.github.com>
2023-01-06 15:33:10 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,148 +1,144 @@
-function _0x541c(_0x5ae325, _0xeea544) {
-    const _0x596e32 = _0x596e();
-    return _0x541c = function (_0x541c52, _0x350346) {
-        _0x541c52 = _0x541c52 - 0x144;
-        let _0x3b7ee9 = _0x596e32[_0x541c52];
-        return _0x3b7ee9;
-    }, _0x541c(_0x5ae325, _0xeea544);
-}
-const _0x520373 = _0x541c;
-function _0x596e() {
-    const _0x515518 = [
-        'sponge',
-        'indexOf',
-        '16288exyeQQ',
-        '740SgkROi',
-        '420HNUrGa',
-        '11255870XxHtYK',
-        'You\x20lose!',
-        'Invalid\x20choice!',
-        '4bliAah',
-        '242163wPQoou',
-        '338889IyZajU',
-        'onclick',
-        'random',
-        'Your\x20opponent\x20chose\x20',
-        'Your\x20choice:\x20',
-        'rock',
-        'Please\x20read\x20\x22How\x20to\x20Play?\x22\x20for\x20instructions.',
-        'paper',
-        '488mKvsMZ',
-        'length',
-        'scissors',
-        'getElementById',
-        'You\x20chose\x20',
-        'Rock\x20Paper\x20Scissors\x20Two:\x20A\x20minigame\x20made\x20by\x20Soy\x20Milk.',
-        'Yes',
-        '30sghDNc',
-        'devil',
-        '1317694FFgnGy',
-        'dragon',
-        'lightning',
-        'water',
-        'Restarting...',
-        '38673wybEJO',
-        'gun',
-        'wolf',
-        'You\x20win!',
-        'air',
-        'Ending\x20game...',
-        '336895eGiVbo',
-        'toLowerCase',
-        'Would\x20you\x20like\x20to\x20play\x20again?',
-        'playButton',
-        'yes',
-        '11YtUflO',
-        'Tie\x20game!'
-    ];
-    _0x596e = function () {
-        return _0x515518;
-    };
-    return _0x596e();
-}
-(function (_0x312fa4, _0x33b40b) {
-    const _0x21aa9b = _0x541c, _0x506999 = _0x312fa4();
+const _0x394b95 = _0x504f;
+(function (_0x38f034, _0x5b73aa) {
+    const _0x53cf13 = _0x504f, _0x586676 = _0x38f034();
     while (!![]) {
         try {
-            const _0x406844 = parseInt(_0x21aa9b(0x169)) / 0x1 + -parseInt(_0x21aa9b(0x167)) / 0x2 * (-parseInt(_0x21aa9b(0x168)) / 0x3) + -parseInt(_0x21aa9b(0x161)) / 0x4 * (-parseInt(_0x21aa9b(0x162)) / 0x5) + parseInt(_0x21aa9b(0x14b)) / 0x6 * (-parseInt(_0x21aa9b(0x14d)) / 0x7) + parseInt(_0x21aa9b(0x144)) / 0x8 * (parseInt(_0x21aa9b(0x152)) / 0x9) + -parseInt(_0x21aa9b(0x164)) / 0xa * (-parseInt(_0x21aa9b(0x15d)) / 0xb) + parseInt(_0x21aa9b(0x163)) / 0xc * (-parseInt(_0x21aa9b(0x158)) / 0xd);
-            if (_0x406844 === _0x33b40b)
+            const _0x159243 = -parseInt(_0x53cf13(0x157)) / 0x1 + -parseInt(_0x53cf13(0x176)) / 0x2 * (-parseInt(_0x53cf13(0x178)) / 0x3) + -parseInt(_0x53cf13(0x17f)) / 0x4 + parseInt(_0x53cf13(0x173)) / 0x5 + -parseInt(_0x53cf13(0x164)) / 0x6 + parseInt(_0x53cf13(0x174)) / 0x7 + parseInt(_0x53cf13(0x162)) / 0x8;
+            if (_0x159243 === _0x5b73aa)
                 break;
             else
-                _0x506999['push'](_0x506999['shift']());
-        } catch (_0x33f414) {
-            _0x506999['push'](_0x506999['shift']());
+                _0x586676['push'](_0x586676['shift']());
+        } catch (_0x14694f) {
+            _0x586676['push'](_0x586676['shift']());
         }
     }
-}(_0x596e, 0x9cd98));
-const playButton = document[_0x520373(0x147)](_0x520373(0x15b));
-playButton[_0x520373(0x16a)] = () => {
-    const _0x274e9 = _0x520373, _0x39f822 = [
-            _0x274e9(0x16e),
+}(_0x99d8, 0x216c8));
+const playButton = document[_0x394b95(0x159)](_0x394b95(0x182)), gameInfoDiv = document['getElementById'](_0x394b95(0x168)), gameInfo = document[_0x394b95(0x159)](_0x394b95(0x16b));
+gameInfo['textContent'] = null, playButton[_0x394b95(0x183)] = () => {
+    const _0x47ae0e = _0x394b95, _0x3e21de = [
+            _0x47ae0e(0x177),
             'fire',
-            _0x274e9(0x146),
-            'snake',
-            'human',
+            _0x47ae0e(0x16d),
+            _0x47ae0e(0x169),
+            _0x47ae0e(0x15b),
             'tree',
-            _0x274e9(0x154),
-            _0x274e9(0x15f),
-            _0x274e9(0x170),
-            _0x274e9(0x156),
-            _0x274e9(0x150),
-            _0x274e9(0x14e),
-            _0x274e9(0x14c),
-            _0x274e9(0x14f),
-            _0x274e9(0x153)
-        ], _0x16565b = [
-            _0x274e9(0x14a),
-            _0x274e9(0x15c),
-            'Y',
-            'y'
+            _0x47ae0e(0x15e),
+            _0x47ae0e(0x170),
+            _0x47ae0e(0x166),
+            _0x47ae0e(0x158),
+            _0x47ae0e(0x16f),
+            _0x47ae0e(0x180),
+            'devil',
+            _0x47ae0e(0x167),
+            _0x47ae0e(0x15c)
         ];
-    function _0x1425ea() {
-        const _0x89b47c = _0x274e9;
-        alert(_0x89b47c(0x149)), alert(_0x89b47c(0x16f));
+    let _0x39f028 = null, _0x317610 = -0x1;
+    do {
+        _0x39f028 = document[_0x47ae0e(0x159)](_0x47ae0e(0x179))[_0x47ae0e(0x16c)]['toLowerCase'](), _0x317610 = _0x3e21de[_0x47ae0e(0x16a)](_0x39f028);
+        if (_0x317610 < 0x0)
+            return gameInfo[_0x47ae0e(0x15f)] = _0x47ae0e(0x17e);
+        ;
+    } while (_0x317610 < 0x0);
+    const _0x3b1bae = _0x52b404(_0x3e21de), _0x5d08c1 = _0x3e21de['indexOf'](_0x3b1bae);
+    gameInfo[_0x47ae0e(0x15f)] = _0x47ae0e(0x165) + _0x39f028 + '.', _0x317610 + 0x1;
+    function _0x5cc887() {
+        const _0x356bc8 = _0x47ae0e;
+        gameInfo[_0x356bc8(0x15f)] = _0x356bc8(0x172) + _0x3b1bae + '.', _0x5d08c1 + 0x1;
     }
     ;
-    _0x1425ea();
-    let _0x1b7d1c = !![], _0x415ec8 = null, _0x2fef4a = -0x1;
-    function _0x33846c() {
-        const _0x2c0a85 = _0x274e9;
-        _0x415ec8 = prompt(_0x2c0a85(0x15a)), _0x415ec8 = _0x415ec8[_0x2c0a85(0x159)](), _0x2fef4a = _0x16565b[_0x2c0a85(0x160)](_0x415ec8);
-        if (_0x2fef4a > 0x0)
-            alert(_0x2c0a85(0x151));
-        else
-            return alert(_0x2c0a85(0x157)), _0x1b7d1c = ![];
-        ;
-        return _0x415ec8;
+    setTimeout(_0x5cc887, 0x7d0), _0x20c3b9();
+    function _0x14556a() {
+        const _0x467bd6 = _0x47ae0e, _0x952e4c = _0x5d08c1 > _0x317610 && _0x5d08c1 <= _0x317610 + 0x7, _0x4ddfaa = _0x952e4c || _0x5d08c1 <= _0x317610 - 0x8, _0x713e7a = _0x317610 <= 0x7 ? _0x952e4c : _0x4ddfaa;
+        gameInfo[_0x467bd6(0x15f)] = _0x713e7a ? 'You\x20win!' : _0x467bd6(0x15a);
     }
     ;
-    while (_0x1b7d1c == !![]) {
-        let _0x1c5b79 = null, _0x5ccd07 = -0x1;
-        do {
-            _0x1c5b79 = prompt(_0x274e9(0x16d)), _0x1c5b79 = _0x1c5b79['toLowerCase'](), _0x5ccd07 = _0x39f822[_0x274e9(0x160)](_0x1c5b79);
-            _0x5ccd07 < 0x0 && alert(_0x274e9(0x166));
-            ;
-        } while (_0x5ccd07 < 0x0);
-        const _0x20b52a = _0x3478c8(_0x39f822), _0x136a5b = _0x39f822[_0x274e9(0x160)](_0x20b52a);
-        alert(_0x274e9(0x148) + _0x1c5b79 + '.', _0x5ccd07 + 0x1), alert(_0x274e9(0x16c) + _0x20b52a + '.', _0x136a5b + 0x1), _0x1aa4bc();
-        function _0x4f441e() {
-            const _0x1f6781 = _0x274e9, _0x3573a1 = _0x136a5b > _0x5ccd07 && _0x136a5b <= _0x5ccd07 + 0x7, _0x36aa8c = _0x3573a1 || _0x136a5b <= _0x5ccd07 - 0x8, _0x3d2b86 = _0x5ccd07 <= 0x7 ? _0x3573a1 : _0x36aa8c;
-            alert(_0x3d2b86 ? _0x1f6781(0x155) : _0x1f6781(0x165));
-        }
+    function _0x537eb7() {
+        const _0x484ffc = _0x47ae0e;
+        gameInfo['innerHTML'] = _0x484ffc(0x17c);
+    }
+    function _0x20c3b9() {
+        if (_0x317610 == _0x5d08c1)
+            return setTimeout(_0x537eb7, 0xfa0);
         ;
-        function _0x1aa4bc() {
-            const _0xe85f24 = _0x274e9;
-            if (_0x5ccd07 == _0x136a5b)
-                return alert(_0xe85f24(0x15e)), _0x33846c();
-            ;
-            _0x4f441e(), _0x33846c();
-        }
-        ;
-        function _0x3478c8(_0x1ec591) {
-            const _0x49c4b0 = _0x274e9;
-            return _0x1ec591[Math['floor'](Math[_0x49c4b0(0x16b)]() * _0x1ec591[_0x49c4b0(0x145)])];
-        }
-        ;
+        setTimeout(_0x14556a, 0xfa0);
+    }
+    ;
+    function _0x52b404(_0x1780c9) {
+        const _0x4e418f = _0x47ae0e;
+        return _0x1780c9[Math[_0x4e418f(0x163)](Math[_0x4e418f(0x16e)]() * _0x1780c9['length'])];
     }
     ;
 };
+const helpButton = document[_0x394b95(0x159)]('helpButton'), helpDiv = document[_0x394b95(0x159)]('helpDiv');
+helpDiv[_0x394b95(0x17b)]['display'] = _0x394b95(0x175), helpButton[_0x394b95(0x183)] = () => {
+    const _0x5e61e5 = _0x394b95;
+    helpDiv[_0x5e61e5(0x17b)][_0x5e61e5(0x15d)] == _0x5e61e5(0x175) ? (helpDiv[_0x5e61e5(0x17b)]['display'] = _0x5e61e5(0x161), helpButton[_0x5e61e5(0x181)] = 'Hide\x20Help\x20Menu') : (helpDiv[_0x5e61e5(0x17b)][_0x5e61e5(0x15d)] = _0x5e61e5(0x175), helpButton[_0x5e61e5(0x181)] = 'Show\x20Help\x20Menu');
+    ;
+};
+function _0x504f(_0x3151b2, _0x4911b1) {
+    const _0x99d889 = _0x99d8();
+    return _0x504f = function (_0x504f27, _0x55c30c) {
+        _0x504f27 = _0x504f27 - 0x157;
+        let _0xd54b6f = _0x99d889[_0x504f27];
+        return _0xd54b6f;
+    }, _0x504f(_0x3151b2, _0x4911b1);
+}
+const creditButton = document[_0x394b95(0x159)](_0x394b95(0x171)), creditDiv = document[_0x394b95(0x159)](_0x394b95(0x160));
+creditDiv['style']['display'] = _0x394b95(0x175), creditButton[_0x394b95(0x183)] = () => {
+    const _0x47fb7f = _0x394b95;
+    creditDiv[_0x47fb7f(0x17b)]['display'] == _0x47fb7f(0x175) ? (creditDiv[_0x47fb7f(0x17b)][_0x47fb7f(0x15d)] = 'block', creditButton['textContent'] = _0x47fb7f(0x17a)) : (creditDiv[_0x47fb7f(0x17b)][_0x47fb7f(0x15d)] = 'none', creditButton['textContent'] = _0x47fb7f(0x17d));
+    ;
+};
+function _0x99d8() {
+    const _0x3c17a9 = [
+        'lightning',
+        'gameInfoDiv',
+        'snake',
+        'indexOf',
+        'gameInfo',
+        'value',
+        'scissors',
+        'random',
+        'water',
+        'sponge',
+        'creditButton',
+        'Your\x20opponent\x20chose\x20',
+        '745295ecbAkn',
+        '888195gPBbOC',
+        'none',
+        '40432TRgBgI',
+        'rock',
+        '21mPmEjw',
+        'userChoice',
+        'Hide\x20Credit',
+        'style',
+        'Tie\x20game!',
+        'Show\x20Credit',
+        'Invalid\x20choice!',
+        '887756bkOBtW',
+        'dragon',
+        'textContent',
+        'playButton',
+        'onclick',
+        '207158ggVjJn',
+        'air',
+        'getElementById',
+        'You\x20lose!',
+        'human',
+        'gun',
+        'display',
+        'wolf',
+        'innerHTML',
+        'creditDiv',
+        'block',
+        '2434592BncPFz',
+        'floor',
+        '934674NqRRiI',
+        'You\x20chose\x20',
+        'paper'
+    ];
+    _0x99d8 = function () {
+        return _0x3c17a9;
+    };
+    return _0x99d8();
+}
