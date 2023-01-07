f2b99bfea603b1eb32d4bcb8ca8e4ee6c6bad646
SoyMilk13 <119906029+SoyMilk13@users.noreply.github.com>
2023-01-07 15:13:42 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,144 +1,165 @@
-const _0x394b95 = _0x504f;
-(function (_0x38f034, _0x5b73aa) {
-    const _0x53cf13 = _0x504f, _0x586676 = _0x38f034();
+const _0x54dcc0 = _0x3a39;
+(function (_0x42c3b8, _0x549b6f) {
+    const _0x435004 = _0x3a39, _0x5eb072 = _0x42c3b8();
     while (!![]) {
         try {
-            const _0x159243 = -parseInt(_0x53cf13(0x157)) / 0x1 + -parseInt(_0x53cf13(0x176)) / 0x2 * (-parseInt(_0x53cf13(0x178)) / 0x3) + -parseInt(_0x53cf13(0x17f)) / 0x4 + parseInt(_0x53cf13(0x173)) / 0x5 + -parseInt(_0x53cf13(0x164)) / 0x6 + parseInt(_0x53cf13(0x174)) / 0x7 + parseInt(_0x53cf13(0x162)) / 0x8;
-            if (_0x159243 === _0x5b73aa)
+            const _0xcbc832 = -parseInt(_0x435004(0x15b)) / 0x1 + -parseInt(_0x435004(0x171)) / 0x2 + -parseInt(_0x435004(0x155)) / 0x3 * (parseInt(_0x435004(0x157)) / 0x4) + -parseInt(_0x435004(0x16c)) / 0x5 + -parseInt(_0x435004(0x15f)) / 0x6 * (-parseInt(_0x435004(0x141)) / 0x7) + parseInt(_0x435004(0x166)) / 0x8 * (parseInt(_0x435004(0x144)) / 0x9) + parseInt(_0x435004(0x14a)) / 0xa;
+            if (_0xcbc832 === _0x549b6f)
                 break;
             else
-                _0x586676['push'](_0x586676['shift']());
-        } catch (_0x14694f) {
-            _0x586676['push'](_0x586676['shift']());
+                _0x5eb072['push'](_0x5eb072['shift']());
+        } catch (_0x1b09e8) {
+            _0x5eb072['push'](_0x5eb072['shift']());
         }
     }
-}(_0x99d8, 0x216c8));
-const playButton = document[_0x394b95(0x159)](_0x394b95(0x182)), gameInfoDiv = document['getElementById'](_0x394b95(0x168)), gameInfo = document[_0x394b95(0x159)](_0x394b95(0x16b));
-gameInfo['textContent'] = null, playButton[_0x394b95(0x183)] = () => {
-    const _0x47ae0e = _0x394b95, _0x3e21de = [
-            _0x47ae0e(0x177),
-            'fire',
-            _0x47ae0e(0x16d),
-            _0x47ae0e(0x169),
-            _0x47ae0e(0x15b),
-            'tree',
-            _0x47ae0e(0x15e),
-            _0x47ae0e(0x170),
-            _0x47ae0e(0x166),
-            _0x47ae0e(0x158),
-            _0x47ae0e(0x16f),
-            _0x47ae0e(0x180),
-            'devil',
-            _0x47ae0e(0x167),
-            _0x47ae0e(0x15c)
-        ];
-    let _0x39f028 = null, _0x317610 = -0x1;
-    do {
-        _0x39f028 = document[_0x47ae0e(0x159)](_0x47ae0e(0x179))[_0x47ae0e(0x16c)]['toLowerCase'](), _0x317610 = _0x3e21de[_0x47ae0e(0x16a)](_0x39f028);
-        if (_0x317610 < 0x0)
-            return gameInfo[_0x47ae0e(0x15f)] = _0x47ae0e(0x17e);
-        ;
-    } while (_0x317610 < 0x0);
-    const _0x3b1bae = _0x52b404(_0x3e21de), _0x5d08c1 = _0x3e21de['indexOf'](_0x3b1bae);
-    gameInfo[_0x47ae0e(0x15f)] = _0x47ae0e(0x165) + _0x39f028 + '.', _0x317610 + 0x1;
-    function _0x5cc887() {
-        const _0x356bc8 = _0x47ae0e;
-        gameInfo[_0x356bc8(0x15f)] = _0x356bc8(0x172) + _0x3b1bae + '.', _0x5d08c1 + 0x1;
-    }
-    ;
-    setTimeout(_0x5cc887, 0x7d0), _0x20c3b9();
-    function _0x14556a() {
-        const _0x467bd6 = _0x47ae0e, _0x952e4c = _0x5d08c1 > _0x317610 && _0x5d08c1 <= _0x317610 + 0x7, _0x4ddfaa = _0x952e4c || _0x5d08c1 <= _0x317610 - 0x8, _0x713e7a = _0x317610 <= 0x7 ? _0x952e4c : _0x4ddfaa;
-        gameInfo[_0x467bd6(0x15f)] = _0x713e7a ? 'You\x20win!' : _0x467bd6(0x15a);
-    }
-    ;
-    function _0x537eb7() {
-        const _0x484ffc = _0x47ae0e;
-        gameInfo['innerHTML'] = _0x484ffc(0x17c);
-    }
-    function _0x20c3b9() {
-        if (_0x317610 == _0x5d08c1)
-            return setTimeout(_0x537eb7, 0xfa0);
-        ;
-        setTimeout(_0x14556a, 0xfa0);
-    }
-    ;
-    function _0x52b404(_0x1780c9) {
-        const _0x4e418f = _0x47ae0e;
-        return _0x1780c9[Math[_0x4e418f(0x163)](Math[_0x4e418f(0x16e)]() * _0x1780c9['length'])];
-    }
-    ;
-};
-const helpButton = document[_0x394b95(0x159)]('helpButton'), helpDiv = document[_0x394b95(0x159)]('helpDiv');
-helpDiv[_0x394b95(0x17b)]['display'] = _0x394b95(0x175), helpButton[_0x394b95(0x183)] = () => {
-    const _0x5e61e5 = _0x394b95;
-    helpDiv[_0x5e61e5(0x17b)][_0x5e61e5(0x15d)] == _0x5e61e5(0x175) ? (helpDiv[_0x5e61e5(0x17b)]['display'] = _0x5e61e5(0x161), helpButton[_0x5e61e5(0x181)] = 'Hide\x20Help\x20Menu') : (helpDiv[_0x5e61e5(0x17b)][_0x5e61e5(0x15d)] = _0x5e61e5(0x175), helpButton[_0x5e61e5(0x181)] = 'Show\x20Help\x20Menu');
-    ;
-};
-function _0x504f(_0x3151b2, _0x4911b1) {
-    const _0x99d889 = _0x99d8();
-    return _0x504f = function (_0x504f27, _0x55c30c) {
-        _0x504f27 = _0x504f27 - 0x157;
-        let _0xd54b6f = _0x99d889[_0x504f27];
-        return _0xd54b6f;
-    }, _0x504f(_0x3151b2, _0x4911b1);
-}
-const creditButton = document[_0x394b95(0x159)](_0x394b95(0x171)), creditDiv = document[_0x394b95(0x159)](_0x394b95(0x160));
-creditDiv['style']['display'] = _0x394b95(0x175), creditButton[_0x394b95(0x183)] = () => {
-    const _0x47fb7f = _0x394b95;
-    creditDiv[_0x47fb7f(0x17b)]['display'] == _0x47fb7f(0x175) ? (creditDiv[_0x47fb7f(0x17b)][_0x47fb7f(0x15d)] = 'block', creditButton['textContent'] = _0x47fb7f(0x17a)) : (creditDiv[_0x47fb7f(0x17b)][_0x47fb7f(0x15d)] = 'none', creditButton['textContent'] = _0x47fb7f(0x17d));
-    ;
-};
-function _0x99d8() {
-    const _0x3c17a9 = [
-        'lightning',
-        'gameInfoDiv',
-        'snake',
-        'indexOf',
-        'gameInfo',
-        'value',
-        'scissors',
-        'random',
-        'water',
-        'sponge',
-        'creditButton',
-        'Your\x20opponent\x20chose\x20',
-        '745295ecbAkn',
-        '888195gPBbOC',
-        'none',
-        '40432TRgBgI',
-        'rock',
-        '21mPmEjw',
-        'userChoice',
-        'Hide\x20Credit',
-        'style',
-        'Tie\x20game!',
-        'Show\x20Credit',
-        'Invalid\x20choice!',
-        '887756bkOBtW',
-        'dragon',
-        'textContent',
-        'playButton',
-        'onclick',
-        '207158ggVjJn',
-        'air',
-        'getElementById',
-        'You\x20lose!',
-        'human',
+}(_0xc733, 0x9a036));
+const playButton = document['getElementById']('playButton'), gameInfoDiv = document[_0x54dcc0(0x169)](_0x54dcc0(0x15a)), gameInfo = document[_0x54dcc0(0x169)](_0x54dcc0(0x13c));
+function _0xc733() {
+    const _0x26777f = [
+        '25587820fAmZEE',
+        'Hide\x20Help\x20Menu',
         'gun',
-        'display',
+        'Hide\x20Credit',
+        'userChoice',
+        'style',
+        'Disable\x20Dark\x20Mode\x20(Beta)',
+        'indexOf',
+        'body',
+        'snake',
+        'helpButton',
+        '3oAQWcv',
+        'scissors',
+        '2168308IiBBmw',
+        'tree',
+        'fire',
+        'gameInfoDiv',
+        '536453SmDply',
+        'helpDiv',
+        'Invalid\x20choice!',
+        'random',
+        '1224ZpQmLe',
+        'color',
+        'air',
+        'Your\x20opponent\x20chose\x20',
+        'You\x20chose\x20',
+        'human',
+        'block',
+        '38336pjFzEH',
+        'none',
+        'textContent',
+        'getElementById',
         'wolf',
+        'devil',
+        '5071745SVVIsW',
+        'floor',
+        'Tie\x20game!',
+        'rock',
+        '#000000',
+        '996714ygsekR',
+        'water',
+        'dragon',
+        '#ffffff',
+        'Show\x20Credit',
+        'creditButton',
+        'gameInfo',
+        'Enable\x20Dark\x20Mode\x20(Beta)',
+        'display',
+        'paper',
+        'backgroundColor',
+        '4837pqHOSJ',
+        'toLowerCase',
+        'onclick',
+        '981brMYdX',
+        'dmButton',
+        'value',
         'innerHTML',
         'creditDiv',
-        'block',
-        '2434592BncPFz',
-        'floor',
-        '934674NqRRiI',
-        'You\x20chose\x20',
-        'paper'
+        'You\x20lose!'
     ];
-    _0x99d8 = function () {
-        return _0x3c17a9;
+    _0xc733 = function () {
+        return _0x26777f;
     };
-    return _0x99d8();
+    return _0xc733();
 }
+gameInfo['textContent'] = null, playButton[_0x54dcc0(0x143)] = () => {
+    const _0xda1923 = _0x54dcc0, _0x5469c0 = [
+            _0xda1923(0x16f),
+            _0xda1923(0x159),
+            _0xda1923(0x156),
+            _0xda1923(0x153),
+            _0xda1923(0x164),
+            _0xda1923(0x158),
+            _0xda1923(0x16a),
+            'sponge',
+            _0xda1923(0x13f),
+            _0xda1923(0x161),
+            _0xda1923(0x172),
+            _0xda1923(0x173),
+            _0xda1923(0x16b),
+            'lightning',
+            _0xda1923(0x14c)
+        ];
+    let _0x427c4e = null, _0x5cc8bf = -0x1;
+    do {
+        _0x427c4e = document[_0xda1923(0x169)](_0xda1923(0x14e))[_0xda1923(0x146)][_0xda1923(0x142)](), _0x5cc8bf = _0x5469c0[_0xda1923(0x151)](_0x427c4e);
+        if (_0x5cc8bf < 0x0)
+            return gameInfo[_0xda1923(0x147)] = _0xda1923(0x15d);
+        ;
+    } while (_0x5cc8bf < 0x0);
+    const _0x201699 = _0x233401(_0x5469c0), _0x2444ed = _0x5469c0['indexOf'](_0x201699);
+    gameInfo['innerHTML'] = _0xda1923(0x163) + _0x427c4e + '.', _0x5cc8bf + 0x1;
+    function _0x1447df() {
+        const _0x6475e0 = _0xda1923;
+        gameInfo[_0x6475e0(0x147)] = _0x6475e0(0x162) + _0x201699 + '.', _0x2444ed + 0x1;
+    }
+    ;
+    setTimeout(_0x1447df, 0x7d0), _0x338ba0();
+    function _0x1a4a18() {
+        const _0x39d85e = _0xda1923, _0x4f5c89 = _0x2444ed > _0x5cc8bf && _0x2444ed <= _0x5cc8bf + 0x7, _0x2c6e7b = _0x4f5c89 || _0x2444ed <= _0x5cc8bf - 0x8, _0x57b942 = _0x5cc8bf <= 0x7 ? _0x4f5c89 : _0x2c6e7b;
+        gameInfo[_0x39d85e(0x147)] = _0x57b942 ? 'You\x20win!' : _0x39d85e(0x149);
+    }
+    ;
+    function _0x183734() {
+        const _0x217e8b = _0xda1923;
+        gameInfo[_0x217e8b(0x147)] = _0x217e8b(0x16e);
+    }
+    function _0x338ba0() {
+        if (_0x5cc8bf == _0x2444ed)
+            return setTimeout(_0x183734, 0xfa0);
+        ;
+        setTimeout(_0x1a4a18, 0xfa0);
+    }
+    ;
+    function _0x233401(_0x173c23) {
+        const _0x3ca8fd = _0xda1923;
+        return _0x173c23[Math[_0x3ca8fd(0x16d)](Math[_0x3ca8fd(0x15e)]() * _0x173c23['length'])];
+    }
+    ;
+};
+const helpButton = document[_0x54dcc0(0x169)](_0x54dcc0(0x154)), helpDiv = document[_0x54dcc0(0x169)](_0x54dcc0(0x15c));
+helpDiv[_0x54dcc0(0x14f)][_0x54dcc0(0x13e)] = _0x54dcc0(0x167), helpButton['onclick'] = () => {
+    const _0x172114 = _0x54dcc0;
+    helpDiv[_0x172114(0x14f)][_0x172114(0x13e)] == _0x172114(0x167) ? (helpDiv[_0x172114(0x14f)][_0x172114(0x13e)] = _0x172114(0x165), helpButton['textContent'] = _0x172114(0x14b)) : (helpDiv[_0x172114(0x14f)]['display'] = _0x172114(0x167), helpButton[_0x172114(0x168)] = 'Show\x20Help\x20Menu');
+    ;
+};
+function _0x3a39(_0x41466b, _0x39378) {
+    const _0xc73392 = _0xc733();
+    return _0x3a39 = function (_0x3a3922, _0x269c64) {
+        _0x3a3922 = _0x3a3922 - 0x13c;
+        let _0x31dbed = _0xc73392[_0x3a3922];
+        return _0x31dbed;
+    }, _0x3a39(_0x41466b, _0x39378);
+}
+const creditButton = document['getElementById'](_0x54dcc0(0x176)), creditDiv = document[_0x54dcc0(0x169)](_0x54dcc0(0x148));
+creditDiv['style'][_0x54dcc0(0x13e)] = _0x54dcc0(0x167), creditButton[_0x54dcc0(0x143)] = () => {
+    const _0x20c92b = _0x54dcc0;
+    creditDiv['style']['display'] == _0x20c92b(0x167) ? (creditDiv[_0x20c92b(0x14f)]['display'] = 'block', creditButton[_0x20c92b(0x168)] = _0x20c92b(0x14d)) : (creditDiv[_0x20c92b(0x14f)][_0x20c92b(0x13e)] = _0x20c92b(0x167), creditButton['textContent'] = _0x20c92b(0x175));
+    ;
+};
+const dmButton = document['getElementById'](_0x54dcc0(0x145)), title = document[_0x54dcc0(0x169)]('title');
+let darkMode = ![];
+dmButton[_0x54dcc0(0x143)] = () => {
+    const _0x441084 = _0x54dcc0;
+    return darkMode == ![] ? (document[_0x441084(0x152)][_0x441084(0x14f)][_0x441084(0x140)] = _0x441084(0x170), document[_0x441084(0x152)][_0x441084(0x14f)][_0x441084(0x160)] = '#ffffff', title[_0x441084(0x14f)]['color'] = _0x441084(0x174), dmButton[_0x441084(0x168)] = _0x441084(0x150), darkMode = !![]) : (document[_0x441084(0x152)]['style'][_0x441084(0x140)] = '#ffffff', document[_0x441084(0x152)]['style'][_0x441084(0x160)] = _0x441084(0x170), title['style']['color'] = '#000000', dmButton[_0x441084(0x168)] = _0x441084(0x13d), darkMode = ![]);
+    ;
+};
